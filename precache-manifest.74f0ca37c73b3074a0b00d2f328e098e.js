09e722da9b40712c942f3a43cfb279599821d329
刘春雷 <lz18739402013@163.com>
2018-11-21 13:56:28 +0800

    upload index.html

---
+++ precache-manifest.74f0ca37c73b3074a0b00d2f328e098e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf64366b40f2be799923",
+    "url": "/react-pwa/static/js/runtime~main.bf64366b.js"
+  },
+  {
+    "revision": "e6418113f176f5446fac",
+    "url": "/react-pwa/static/js/main.e6418113.chunk.js"
+  },
+  {
+    "revision": "f4f3ceda2cd3badd5973",
+    "url": "/react-pwa/static/js/1.f4f3ceda.chunk.js"
+  },
+  {
+    "revision": "a3a5125b44343a56879475bd38e78566",
+    "url": "/react-pwa/index.html"
+  }
+];
